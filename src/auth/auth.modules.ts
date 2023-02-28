800d4ecec1e691ac5b1bbf506e7b97fe67b4226e
sg <396857483@qq.com>
2023-02-28 23:26:01 +0800

    add jwt
    add jwt

--- src/auth/auth.modules.ts
+++ src/auth/auth.modules.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { LocalStrategy } from './local.strategy';
 import { User } from './user.entity';
+import { AUTH_SECRET } from '../config/options.config';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [LocalStrategy],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: AUTH_SECRET,
+      signOptions: { expiresIn: '60m' },
+    }),
+  ],
+  providers: [LocalStrategy, JwtStrategy, AuthService],
   controllers: [AuthController],
 })
 export class AuthModule {}
---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { AUTH_SECRET } from '../config/options.config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: AUTH_SECRET,
+    });
+  }
+
+  async validate(payload: any) {
+    return await this.userRepository.findOne({ where: payload.sub });
+  }
+}
